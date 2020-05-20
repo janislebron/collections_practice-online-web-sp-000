f42a3c1d67d9db6af9ef417e0a7c8f0dcc3ccbf3
Janis Lebron <janislebron@gmail.com>
2020-05-20 23:23:59 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,30 @@
+def sort_array_asc(integers)
+  integers.sort do |a, b|
+    a <=> b
+  end
+end
 
+def sort_array_desc(integers)
+  integers.sort do |a, b|
+    if a == b
+    0
+  elsif a > b
+    -1
+  elsif a < b
+    1
+  end
+end
+
+def sort_array_char_count(strings)
+  strings.sort do |a, b|
+    if a == b
+    0
+  elsif a < b
+    -1
+  elsif a > b
+    1
+  end
+  end
+end
+
+def swap_elements
